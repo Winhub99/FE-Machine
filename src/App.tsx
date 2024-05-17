c749296ef762b752ec9c70b0eab1702f62333e2f
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-05-17 16:40:24 +0530

    2nd step completed

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import './App.css'
 
 function App() {
   const [todos, setTodos] = useState([])
-  type todo={
+  type todoType={
     completed:boolean,
     id: number,
     todo:string,
     userId:number
   }
 
+  type newDS ={
+    userId:number,
+    todos:string[]
+  }
   useEffect(()=>{
     const fetchTodos = async()=>{
       const res = await fetch('https://dummyjson.com/todos?limit=10&skip=80')
@@ -22,10 +26,21 @@ function App() {
     fetchTodos()
   },[])
 
+  const newDS= todos.reduce((accumulator,curr:todoType)=>{
+      const {userId,todo} = curr;
+      if(!accumulator[userId]){
+        accumulator[userId]=[];
+      }
+      accumulator[userId].push(todo)
+      
+    return accumulator;
+  },{})
+  console.log(newDS);
+  
   return (
     <div>
         <h2>Todos</h2>
-        {todos.map((myTodo:todo)=>(
+        {todos.map((myTodo:todoType)=>(
           <div key={myTodo.id}>
             <h1>{myTodo?.userId}</h1>
             <h4>
