f19a121e434271f4052e9dfb59d382afe82f1c5f
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-05-23 18:46:05 +0530

    step 1 completed

---
+++ src/components/FifthQuestion.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+
+const FifthQuestion = () => {
+    const [products,setProducts] = useState([])
+
+    useEffect(()=>{
+        const fetchProducts= async()=>{
+            const response = await fetch('https://dummyjson.com/products')
+            const data = await response.json()
+            console.log(data);
+            setProducts(data.products)
+        }
+        fetchProducts()
+    },[])
+  return (
+    <div>
+      Pagination
+      {products.length > 0 && <div className="products">
+          {products.map((product)=>{
+            return (<span className="products__single" key={product.id}>
+              <img src={product.thumbnail} alt={product.title}/>
+              <span className="title">{product.title}</span>
+      
+              </span>)
+          })}
+          </div>}
+    </div>
+  )
+}
+
+export default FifthQuestion
