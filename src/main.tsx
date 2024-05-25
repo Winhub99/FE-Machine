0d4b512aba9524d7d5dc2ce6bcde214cb46425cc
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-05-25 18:15:12 +0530

    running app

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from './App.tsx'
 import './index.css'
 import ThirdQuestion from './components/ThirdQuestion.tsx'
 import FifthQuestion from './components/FifthQuestion.tsx'
+import MainComponent from './components/fifthQuestion/MainComponent.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <ThirdQuestion/> */}
-    <FifthQuestion/>
+    {/* <FifthQuestion/> */}
+    <MainComponent/>
   </React.StrictMode>,
 )
