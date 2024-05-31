a71fbe4ace02fdfa401d28cf9347fbc7499c70db
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-05-31 17:51:01 +0530

    rough solution

--- src/components/sixthQuestion/Stopwatch.tsx
+++ src/components/sixthQuestion/Stopwatch.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Stopwatch = () => {
+    const [seconds,setSeconds] = useState(0)
+    const [minutes,setMinutes] = useState(0)
+    const [hours,setHours] = useState(0)
+
+    const startHandler=()=>{
+        setInterval(()=>{
+            console.log('the seconds are : ',seconds);
+            
+            if(seconds===60){
+                if(minutes===60){
+                    setHours(prev=>prev+1)
+                    setMinutes(0)
+                }else{
+                    setMinutes(prev=> prev+1)
+
+                }
+                setSeconds(0)
+            }
+            setSeconds(prev=>prev+1)
+        },1000)
+    }
   return (
+
     <div>
       <h2>Stopwatch</h2>
+      <div>
+        <h4>{hours}::{minutes}::{seconds}</h4>
+      </div>
+      <div style={{display:'flex', gap:'10px'}}>
+        <button>Stop</button>
+        <button onClick={startHandler}>Start</button>
+        <button>Clear</button>
+      </div>
     </div>
   )
 }
