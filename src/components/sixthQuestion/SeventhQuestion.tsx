0a0971fd1ab5bca5c1102639eab0c1e06af7d794
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-06-03 18:35:43 +0530

    full solution with two vas

--- src/components/sixthQuestion/SeventhQuestion.tsx
+++ src/components/sixthQuestion/SeventhQuestion.tsx
@@ -2,12 +2,18 @@ import React, { useState } from 'react'
 
 const SeventhQuestion = () => {
     const [count,setCount] = useState(0)
+    const [prevoius,setPrevious] = useState(0)
+    const incrementCounterHandler=()=>{
+         setPrevious(count)        
+        setCount(prev=>prev+1)
+    }
   return (
     <div>
         <h2>Counter</h2>
         <div>
-            {count}
-            <button>Increment</button>
+            <span><b>Current:</b></span>{count}
+            <span><b>Prev:</b></span>{prevoius}
+            <button onClick={incrementCounterHandler}>Increment</button>
         </div>
     </div>
   )
