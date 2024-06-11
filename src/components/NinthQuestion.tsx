290865e4b09c5b3f041e426c3bfd5242e582725a
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-06-11 19:25:29 +0530

    step 2 3 completed

--- src/components/NinthQuestion.tsx
+++ src/components/NinthQuestion.tsx
@@ -2,18 +2,38 @@ import React, { useState } from 'react'
 
 const NinthQuestion = ({dataType}) => {
 
-  const [inputValue,setInputValue] = useState()
+  const [inputValue,setInputValue] = useState(""||0)
+  const [error,setError] = useState("")
   const validate=(e)=>{
     console.log('the data: ',dataType);
+    setInputValue(e.target.value)
+
+    if(dataType==="number"){
+      if(inputValue > 999 || inputValue < 0 ){
+        
+        setError("Number should be in range of 0 to 999")
+
+      }
+    }else{
+      if(dataType==="text"){
+        if(inputValue.length> 15){
+          console.log("char length error occured");
+
+          setError('Max 15 characters allowed')
+        }
+      }
+    }
     
     
   }
   return (
     <div>
+    {error ? (<div> {error}</div>): (<div>
       <h2>Type Checker</h2>
       <input type={dataType} onChange={(e)=>validate(e)} value={inputValue} />
+    </div>)
+    }
     </div>
   )
 }
-
 export default NinthQuestion
