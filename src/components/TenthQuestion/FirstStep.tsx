205f31bfcce72399c8b5a776ffcb1c8c96293532
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-06-18 18:50:35 +0530

    step 2 completed

--- src/components/TenthQuestion/FirstStep.tsx
+++ src/components/TenthQuestion/FirstStep.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const FirstStep = ({formData}) => {
+const FirstStep = ({formData,handleChange}) => {
   return (
     <div>
       <h2>Step 1</h2>
-       Firstname: <input type="text" name='firstname' value={formData.firstname} /><br/>
-       Lastname: <input type="text" name='lastname' value={formData.lastname} />
+       Firstname: <input type="text" name='firstname' value={formData.firstname} onChange={handleChange} /><br/>
+       Lastname: <input type="text" name='lastname' value={formData.lastname} onChange={handleChange}/>
     </div>
   )
 }
