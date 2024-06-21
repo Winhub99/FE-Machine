0171a60dcafbef4bafae825b46ffffd360892759
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-06-21 16:53:46 +0530

    all steps in one go

--- src/components/EleventhQuestion.tsx
+++ src/components/EleventhQuestion.tsx
@@ -2,12 +2,22 @@ import React, { useState } from 'react'
 
 const EleventhQuestion = () => {
     const [textInput,setTextInput] = useState('')
-
+    const [modifiedText,setModidfiedText] = useState('')
     const handleChange=(e)=>{
         const str = e.target.value
         setTextInput(str)
         console.log(str);
-        
+
+        let result=''
+        for(let i=0;i<str.length;i++){
+            let char = str.charCodeAt(i)
+            char += 10;
+            result += String.fromCharCode(char)
+
+        }
+        console.log('the result after ascii modification: ', result);
+        setModidfiedText(result)
+
     }
   return (
     <>
@@ -16,8 +26,8 @@ const EleventhQuestion = () => {
       Text:<input type="text" onChange={handleChange} />
     </div>
     <div>
-
-       
+        <h2>Converted Text:</h2>
+        {modifiedText.length>0 && modifiedText}
     </div>
     </>
   )
