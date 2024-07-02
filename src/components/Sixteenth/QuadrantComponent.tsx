4313a264fea5d5a6b651daf4fb602cc91cb2ce47
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-07-02 18:26:55 +0530

    adding handler

--- src/components/Sixteenth/QuadrantComponent.tsx
+++ src/components/Sixteenth/QuadrantComponent.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import  { useState } from 'react'
 
 const QuadrantComponent = () => {
+   
+    const [clicked,setClicked] = useState(false)
+    
+    const clickHandler = ()=>{
+
+        setClicked(true)
+    }
   return (
-    <div style={{width:'50px',height:'50px', backgroundColor:'blue',margin:'2px'}}>
-      
+    <div style={{width:'50px',height:'50px', backgroundColor:'blue',margin:'2px'}} onClick={clickHandler}>
+      {clicked && <h6>Clicked!</h6>}
     </div>
   )
 }
