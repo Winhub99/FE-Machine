0f8bceb4a2225e98989256193388b56b021baf4d
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-07-02 18:13:01 +0530

    layout design

--- src/components/Sixteenth/QuadrantComponent.tsx
+++ src/components/Sixteenth/QuadrantComponent.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const QuadrantComponent = () => {
   return (
-    <div style={{width:'50px',height:'50px', backgroundColor:'blue',margin:'10px'}}>
+    <div style={{width:'50px',height:'50px', backgroundColor:'blue',margin:'2px'}}>
       
     </div>
   )
