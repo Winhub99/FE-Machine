056591e4044cae60a9d4af6ee425196674014dda
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-07-07 18:59:20 +0530

    check

--- src/components/Sixteenth/QuadrantComponent.tsx
+++ src/components/Sixteenth/QuadrantComponent.tsx
@@ -1,4 +1,4 @@
-import  { useState } from 'react'
+import  { useEffect, useState } from 'react'
 
 const QuadrantComponent = () => {
    
@@ -8,6 +8,19 @@ const QuadrantComponent = () => {
 
         setClicked(true)
     }
+
+    useEffect(()=>{
+
+        return ()=> {
+          console.log('the clicked state was false');
+          
+            if(clicked){
+                console.log('the clicked state is : ', clicked);
+                
+            setClicked(false)
+        }
+        }
+    },[clicked])
   return (
     <div style={{width:'50px',height:'50px', backgroundColor:'blue',margin:'2px'}} onClick={clickHandler}>
       {clicked && <h6>Clicked!</h6>}
