5ca94773b95a79d83587e947a50c615bd52703d7
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-07-19 17:54:57 +0530

    removed redundant code

--- src/components/Sixteenth/QuadrantComponent.tsx
+++ src/components/Sixteenth/QuadrantComponent.tsx
@@ -1,25 +1,7 @@
 import  { useEffect, useState } from 'react'
 
 const QuadrantComponent = ({id,isActive,onClick}) => {
-   
-    
-    // const clickHandler = ()=>{
-
-    //     setClicked(true)
-    // }
-
-    // useEffect(()=>{
-
-    //     return ()=> {
-    //       console.log('the clicked state was false');
-          
-    //         if(clicked){
-    //             console.log('the clicked state is : ', clicked);
-                
-    //         setClicked(false)
-    //     }
-    //     }
-    // },[clicked])
+  
   return (
     <div style={{width:'50px',height:'50px', backgroundColor:'blue',margin:'2px'}} onClick={()=>onClick(id)}>
       {isActive && <h6>Clicked!</h6>}
