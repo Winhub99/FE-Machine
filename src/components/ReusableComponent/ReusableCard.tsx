c0d5d78351bf38e275073e265f162ce6886438c3
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-07-30 18:23:32 +0530

    container created

---
+++ src/components/ReusableComponent/ReusableCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './Card'
+
+const ReusableCard = () => {
+  return (
+    <div>
+      <Card header="FaceBook Header" type=""/>
+    </div>
+  )
+}
+
+export default ReusableCard
