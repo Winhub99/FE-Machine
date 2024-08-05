5280249f891081a23e756b0fc7ba24c075cba0fd
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-08-05 17:25:49 +0530

    small change in tect

--- src/components/ReusableComponent/ReusableCard.tsx
+++ src/components/ReusableComponent/ReusableCard.tsx
@@ -4,13 +4,13 @@ const ReusableCard = () => {
     return (
         <div>
             <Card header="Twitter Header" type="Twitter">
-                <p>This is a post on Twitter.</p>
+                <p>This is a post on Twitter!</p>
             </Card>
             <Card header="FaceBook Header" type="Facebook">
-                <p>This is a post on Facebook.</p>
+                <p>This is a post on Facebook!</p>
             </Card>
             <Card header="Default Header" type="Default">
-                <p>This is a default card with no specific type styling.</p>
+                <p>This is a default card with no specific type styling!</p>
             </Card>
 
 
